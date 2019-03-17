a88fe264360b2087612005fb55372af912f2f74e
Farley Reynolds <farleykreynolds@gmail.com>
2019-03-17 08:29:54 -0400

    initial commit

---
+++ shot-css.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import fire
+
+template = """ 
+.text-color-{num} {{
+    color: {color};
+}}
+
+"""
+
+def get_shot_colors(shot_url):
+    source = requests.get(shot_url).text
+    soup = BeautifulSoup(source, 'lxml')
+    colors = soup.find_all('li', class_='color')
+
+    with open('styles.css', 'w+') as f:
+        num = 1
+        
+        for color in colors:
+            color_hex = color.text.strip()
+
+            context = {
+                'num' : num,
+                'color' : color_hex,
+            }
+
+            f.write(template.format(**context))
+            num += 1
+
+
+if __name__ == '__main__':
+    fire.Fire(get_shot_colors)
